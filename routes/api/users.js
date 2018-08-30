d00137e7e7201aac8440d0a137868c4aed8dd583
JudahL <judahdslucas@gmail.com>
2018-08-30 12:28:27 +0100

    Refactored User routes and controllers for greater modularity and seperation of concerns

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,90 +1,21 @@
 const express = require('express');
-const User = require('../../models/User');
+const { userRegisterPost, userSignInPost, userSignOutPost, userGet } = require('../../controllers/UsersController');
 
 const router = express.Router();
 
 /**
  * '/api/users' routes:
  */
-router.post('/register', (req, res, next) => {
 
-  //If all fields aren't supplied then return an error
-  if (!req.body.username || !req.body.password || !req.body.passwordConfirm) {
-    return next(userError('FieldsMustBeFilled', 400));
-  }
+router.get('/get', userGet);
 
-  // Ensure the user has entered the same 'password' and 'confirm password'
-  if (req.body.password !== req.body.passwordConfirm) {
-    return next(userError('PasswordsDoNotMatch', 400));
-  }
+router.post('/register', userRegisterPost);
 
-  const newUser = {
-    username: req.body.username,
-    password: req.body.password
-  }
+router.post('/signin', userSignInPost);
 
-  // Add a new user to the database
-  User.create(newUser, (err, user) => {
-    if (err) {
-      const error = new Error(err.code.toString());
-      return next(error);
-    } else {
-      return res.json(user.username);
-    }
-  });
-});
-
-router.post('/signin', (req, res, next) => {
-  // If either the username or password isn't supplied then return an error
-  if (!req.body.username || !req.body.password) {
-    return next(userError('FieldsMustBeFilled', 400));
-  }
-
-  // Ensure the username and password the user has entered corresponds to a user on the database
-  User.authenticate(req.body.username, req.body.password, (error, user) => {
-    if (error || !user) {
-      return next(userError('WrongUsernameOrPassword', 401));
-    } else {
-      // If authentication is successful set the userId field in the session to be the MongoDB id
-      // This will be used in other routes to check whether a user is already logged in
-      req.session.userId = user._id;
-
-      return res.json(user.username);
-    }
-  });
-});
-
-router.post('/signout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
-});
-
-router.get('/get', (req, res, next) => {
-  User.findById(req.session.userId)
-    .exec()
-    .then(user => {
-      if (user === null) {
-        return res.json('');
-      } else {
-        return res.json(user.username);
-      }
-    })
-    .catch(err => next(err));
-});
+router.post('/signout', userSignOutPost);
 
 
-// Create and return a new error given a specified error message and http status
-function userError(message, status) {
-  const err = new Error(message);
-  err.status = status;
-  return err;
-}
+
 
 module.exports = router;
