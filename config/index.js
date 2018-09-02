76cdbabd144cb3610e077e1414e66ad7aa819a99
JudahL <judahdslucas@gmail.com>
2018-09-02 18:08:02 +0100

    Cleaning up config file

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     productionPort: process.env.PORT,
   },
   db: {
-    host: `mongodb://${/*process.env.MONGO_USER_USERNAME*/'JudahL'}:${/*process.env.MONGO_USER_PASSWORD*/'Firestream1993'}@ds235251.mlab.com`,
+    host: `mongodb://${process.env.MONGO_USER_USERNAME}:${process.env.MONGO_USER_PASSWORD}@ds235251.mlab.com`,
     port: '35251',
     appName: 'objectives_app',
   },
